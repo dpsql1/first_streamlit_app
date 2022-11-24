dd0e7a4ba6fda24c3e55098956161865e5b995a5
dpsql1 <118969556+dpsql1@users.noreply.github.com>
2022-11-24 10:10:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 streamlit streamlit_app.py --server.enableCORS=false
+streamlit run my_app.py --server.enableWebsocketCompression=false
 
 streamlit.title('My Parents New Healthy Diner')
